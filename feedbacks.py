6bb6853681d97dfe9a7212b6ad21fdcf53ea07e8
Vitor Buxbaum <vitor.buxbaum@betrybe.com>
2021-11-04 14:56:08 -0300

    novos métodos para update e build de feedbacks

--- feedbacks.py
+++ feedbacks.py
@@ -3,7 +3,6 @@ import json
 
 class Feedbacks:
     def __init__(self):
-        self.__feedbacks = {}
         self.update_feedbacks()
 
     def get_avoided_expressions(self) -> list:
@@ -14,6 +13,18 @@ class Feedbacks:
             with open("data/feedbacks.json") as feedbacks_file:
                 feedbacks_raw = feedbacks_file.read()
                 self.__feedbacks = json.loads(feedbacks_raw)
+        except FileNotFoundError:
+            raise FileNotFoundError("Base de feedbacks não encontrada")
+        except json.decoder.JSONDecodeError as e:
+            raise SyntaxError(
+                f"Base de feedbacks com sintaxe inválida : {e.args[0]}"
+            )
+
+    def new_update_feedbacks(self) -> None:
+        try:
+            with open("data/feedbacks_new.json") as feedbacks_file:
+                self.__feedbacks = json.load(feedbacks_file)["feedbacks"]
+                self.__default_text = json.load(feedbacks_file)["default_text"]
         except FileNotFoundError:
             raise FileNotFoundError("Base de feedbacks não encontrada")
         except json.decoder.JSONDecodeError as e:
@@ -42,6 +53,24 @@ class Feedbacks:
         explanation = f":eyes: Olha só: {self.__feedbacks[found_word]}"
 
         goodbye = "#VQV"
+
+        return "\n\n".join([intro, explanation, goodbye])
+
+    def new_build_feedback(self, found_word: str, user_id: str) -> str:
+        intro = (
+            self.__default_text["intro"]
+            .replace("<user_id>", user_id)
+            .replace("<found_word>", found_word)
+        )
+
+        with open(f"{self.__feedbacks[found_word]}.slack") as feedback_file:
+            feedback_text = feedback_file.read()
+
+        explanation = self.__default_text["explanation"].replace(
+            "<feedback>", feedback_text
+        )
+
+        goodbye = self.__default_text["goodbye"]
 
         return "\n\n".join([intro, explanation, goodbye])
 
