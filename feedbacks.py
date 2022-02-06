c70bbb384a264620035949c264d113a07595151b
Vitor Buxbaum <vitor.buxbaum@betrybe.com>
2022-02-06 18:44:16 -0300

    novo formato de arquivo único + refatoração class Feedbacks

--- feedbacks.py
+++ feedbacks.py
@@ -2,24 +2,28 @@ import json
 
 
 class Feedbacks:
-    DATA_PATH = "data/"
-    FEEDBACKS_PATH = DATA_PATH + "feedbacks.json"
+    FEEDBACKS_PATH = "feedbacks.json"
 
-    def __init__(self):
-        self._load_feedbacks()
+    def __init__(self) -> None:
+        self.load_feedbacks()
 
-    def _get_avoided_expressions(self) -> list:
-        return list(self.__feedbacks.keys())
+    def load_feedbacks(self) -> None:
+        print("Loading feedbacks...")
+        full_dict = self._read_feedbacks_base()
 
-    def _load_feedbacks(self) -> None:
-        print("loading feedbacks...")
+        self.__feedbacks = full_dict["feedbacks"]
+        self.__default_text = full_dict["default_text"]
+        self.__explanations = full_dict["explanation_patterns"]
+
+    @classmethod
+    def _read_feedbacks_base(cls) -> dict:
         try:
-            with open(self.FEEDBACKS_PATH) as feedbacks_file:
-                full_dict = json.load(feedbacks_file)
-                self.__feedbacks = full_dict["feedbacks"]
-                self.__default_text = full_dict["default_text"]
-        except FileNotFoundError:
-            raise FileNotFoundError("Base de feedbacks não encontrada")
+            with open(cls.FEEDBACKS_PATH) as feedbacks_file:
+                return json.load(feedbacks_file)
+        except FileNotFoundError as e:
+            raise FileNotFoundError(
+                f"Base de feedbacks não encontrada : {e.args[0]}"
+            )
         except json.decoder.JSONDecodeError as e:
             raise SyntaxError(
                 f"Base de feedbacks com sintaxe inválida : {e.args[0]}"
@@ -39,14 +43,17 @@ class Feedbacks:
 
         return None
 
+    def _get_avoided_expressions(self) -> list:
+        return list(self.__feedbacks.keys())
+
     def build_feedback(self, found_word: str, user_id: str) -> str:
-        intro = self._build_intro(found_word, user_id)
-
-        explanation = self._build_explanation(found_word)
-
-        goodbye = self._build_goodbye()
-
-        return "\n\n".join([intro, explanation, goodbye])
+        return "\n\n".join(
+            [
+                self._build_intro(found_word, user_id),
+                self._build_explanation(found_word),
+                self._build_goodbye(),
+            ]
+        )
 
     def _build_intro(self, found_word: str, user_id: str) -> str:
         return (
@@ -56,10 +63,7 @@ class Feedbacks:
         )
 
     def _build_explanation(self, found_word: str) -> str:
-        feedback_path = self.DATA_PATH + self.__feedbacks[found_word]
-
-        with open(feedback_path) as feedback_file:
-            feedback_text = feedback_file.read()
+        feedback_text = self.__explanations[self.__feedbacks[found_word]]
 
         return self.__default_text["explanation"].replace(
             "<feedback>", feedback_text
