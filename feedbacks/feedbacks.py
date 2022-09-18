2df4817580bf4e2db85ef3c95eb6e10e73837681
Vitor Buxbaum <vitorbuxbaum@gmail.com>
2022-09-18 17:34:13 -0300

    remove código da versão Slack + ajusta para informar alternativas

--- feedbacks/feedbacks.py
+++ feedbacks/feedbacks.py
@@ -1,5 +1,11 @@
 import json
 from feedbacks.text_analyzer import TextAnalyzer
+from pydantic import BaseModel
+
+
+class LNGReport(BaseModel):
+    used_expression: str = ""
+    expression_alternatives: list = []
 
 
 class Feedbacks:
@@ -13,8 +19,7 @@ class Feedbacks:
         full_dict = self._read_feedbacks_base()
 
         self.__feedbacks = full_dict["feedbacks"]
-        self.__default_text = full_dict["default_text"]
-        self.__explanations = full_dict["explanation_patterns"]
+        self.__alternatives = full_dict["alternatives"]
 
         print("Feedbacks loaded!")
 
@@ -43,72 +48,14 @@ class Feedbacks:
             )
 
             if found_expression:
-                result.append((found_expression, feedback_pattern))
+
+                result.append(
+                    LNGReport(
+                        used_expression=found_expression,
+                        expression_alternatives=self.__alternatives[
+                            feedback_pattern
+                        ],
+                    )
+                )
 
         return result
-
-    def build_feedback(
-        self,
-        expression_and_pattern: list,
-        user_id: str,
-        thread_link: str,
-    ) -> str:
-        expressions = [expression for expression, _ in expression_and_pattern]
-        patterns = [pattern for _, pattern in expression_and_pattern]
-
-        return "\n\n".join(
-            [
-                self._build_intro(expressions, user_id, thread_link),
-                self._build_explanation(patterns),
-                self._build_goodbye(),
-            ]
-        )
-
-    def _build_intro(
-        self, found_expressions: str, user_id: str, thread_link: str
-    ) -> str:
-
-        expressions_to_mention = self.__expressions_to_mention(
-            found_expressions
-        )
-
-        return (
-            self.__default_text["intro"]
-            .replace("<user_id>", user_id)
-            .replace("<found_expressions>", expressions_to_mention)
-            .replace("<thread_link>", thread_link)
-        )
-
-    def __expressions_to_mention(self, expressions):
-        expressions_to_mention = f'"*{expressions[0]}*"'
-
-        if len(expressions) == 1:
-            return expressions_to_mention
-
-        for expression in expressions[1:-1]:
-            expressions_to_mention += f', "*{expression}*"'
-
-        return expressions_to_mention + f' e "*{expressions[-1]}*"'
-
-    def _build_explanation(self, patterns: str) -> str:
-
-        feedbacks_to_give = self.__feedbacks_to_give(patterns)
-
-        return self.__default_text["explanation"].replace(
-            "<feedback>", feedbacks_to_give
-        )
-
-    def __feedbacks_to_give(self, patterns):
-        if len(patterns) == 1:
-            return self.__explanations[patterns[0]]
-
-        feedbacks_to_give = "\n"
-        for pattern in patterns:
-            feedbacks_to_give += (
-                f"\n:arrow_right: {self.__explanations[pattern]}"
-            )
-
-        return feedbacks_to_give
-
-    def _build_goodbye(self) -> str:
-        return self.__default_text["goodbye"]
