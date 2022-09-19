303416f55f172515aabffb90f4b8a4a0975cdab2
Vitor Buxbaum <vitorbuxbaum@gmail.com>
2022-09-19 15:16:43 -0300

    adiciona endpoint para análise de termos customizados

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from typing import List
 from fastapi import FastAPI, Query
-from feedbacks.feedbacks import Feedbacks, LNGReport
+from feedbacks.feedbacks import AnalyzerOptions, Feedbacks, LNGReport
 
 app = FastAPI(title="LNG API")
 
@@ -10,7 +10,19 @@ feedback_handler = Feedbacks()
 @app.get("/", response_model=List[LNGReport])
 def process_text(
     input_text: str = Query(
-        default="", example="João e Maria são ótimos amigos", lt=10000
+        default="", example="João e Maria são ótimos amigos", max_length=10000
     )
 ):
     return feedback_handler.find_avoided_expression(input_text)
+
+
+@app.post("/", response_model=List[LNGReport])
+def process_text_custom_options(
+    analyzer_options: AnalyzerOptions,
+    input_text: str = Query(
+        default="", example="João e Maria são ótimos amigos", max_length=10000
+    ),
+):
+    return Feedbacks(analyzer_options.dict()).find_avoided_expression(
+        input_text
+    )
